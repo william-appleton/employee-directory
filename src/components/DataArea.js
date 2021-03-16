969523559072be816bc504f2e978012d529d7382
Imprudent Friend <ImprudentFriend@gmail.com>
2021-03-16 16:38:00 -0400

    added styling to header

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -32,26 +32,22 @@ export default class DataArea extends Component {
 
     const compareFnc = (a, b) => {
       if (this.state.order === "ascend") {
-        // account for missing values
         if (a[heading] === undefined) {
           return 1;
         } else if (b[heading] === undefined) {
           return -1;
         }
-        // numerically
         else if (heading === "name") {
           return a[heading].first.localeCompare(b[heading].first);
         } else {
           return a[heading] - b[heading];
         }
       } else {
-        // account for missing values
         if (a[heading] === undefined) {
           return 1;
         } else if (b[heading] === undefined) {
           return -1;
         }
-        // numerically
         else if (heading === "name") {
           return b[heading].first.localeCompare(a[heading].first);
         } else {
@@ -68,7 +64,6 @@ export default class DataArea extends Component {
     console.log(event.target.value);
     const filter = event.target.value;
     const filteredList = this.state.users.filter(item => {
-      // merge data together, then see if user input is anywhere inside
       let values = Object.values(item)
         .join("")
         .toLowerCase();
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+        <h1>Employee Directory!</h1>
+        <p>You can use the search bar to find an exact match, or filter by heading by clicking the carrot next to each heading.</p>
       </div>
     )
   }
