0bbed4bfa9f607345efad78842b4e9dde5ee5c28
Imprudent Friend <ImprudentFriend@gmail.com>
2021-03-16 17:43:16 -0400

    react

---
+++ build/precache-manifest.504d87bab4d902c2e3432fb1b9466ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee006784502a8bb908350a7dd7e3f39e",
+    "url": "/employee-directory/index.html/index.html"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/employee-directory/index.html/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "35ac428fd323eb0144f3",
+    "url": "/employee-directory/index.html/static/css/main.97df5295.chunk.css"
+  },
+  {
+    "revision": "c85635143026af4a75f7",
+    "url": "/employee-directory/index.html/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "35ac428fd323eb0144f3",
+    "url": "/employee-directory/index.html/static/js/main.18fd003a.chunk.js"
+  },
+  {
+    "revision": "279dee273c1af52dceae",
+    "url": "/employee-directory/index.html/static/js/runtime~main.3dbe8fad.js"
+  }
+]);
